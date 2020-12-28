8a13ba2e049b5dcbebd5a66463c3661a43c6ace4
deltafran <deltafran@mail.ru>
2020-12-28 19:50:37 +1000

    - чистка

--- code/TinyRPG/RendererSystem.cpp
+++ code/TinyRPG/RendererSystem.cpp
@@ -29,7 +29,6 @@ bool RendererSystem::Init() noexcept
 	HRESULT result;
 	int error;
 
-	// Create a DirectX graphics interface factory.
 	IDXGIFactory *factory;
 	result = CreateDXGIFactory(__uuidof(IDXGIFactory), (void**)&factory);
 	if (FAILED(result))
@@ -37,7 +36,6 @@ bool RendererSystem::Init() noexcept
 		return false;
 	}
 
-	// Use the factory to create an adapter for the primary graphics interface (video card).
 	IDXGIAdapter* adapter;
 	result = factory->EnumAdapters(0, &adapter);
 	if (FAILED(result))
@@ -45,7 +43,6 @@ bool RendererSystem::Init() noexcept
 		return false;
 	}
 
-	// Enumerate the primary adapter output (monitor).
 	IDXGIOutput* adapterOutput;
 	result = adapter->EnumOutputs(0, &adapterOutput);
 	if (FAILED(result))
@@ -53,7 +50,6 @@ bool RendererSystem::Init() noexcept
 		return false;
 	}
 
-	// Get the number of modes that fit the DXGI_FORMAT_R8G8B8A8_UNORM display format for the adapter output (monitor).
 	unsigned int numModes;
 	result = adapterOutput->GetDisplayModeList(DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numModes, NULL);
 	if (FAILED(result))
@@ -61,22 +57,18 @@ bool RendererSystem::Init() noexcept
 		return false;
 	}
 
-	// Create a list to hold all the possible display modes for this monitor/video card combination.
 	DXGI_MODE_DESC* displayModeList = new DXGI_MODE_DESC[numModes];
 	if (!displayModeList)
 	{
 		return false;
 	}
 
-	// Now fill the display mode list structures.
 	result = adapterOutput->GetDisplayModeList(DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_ENUM_MODES_INTERLACED, &numModes, displayModeList);
 	if (FAILED(result))
 	{
 		return false;
 	}
 
-	// Now go through all the display modes and find the one that matches the screen width and height.
-	// When a match is found store the numerator and denominator of the refresh rate for that monitor.
 	unsigned int numerator = 0, denominator = 1;
 	for (unsigned i = 0; i < numModes; i++)
 	{
@@ -90,56 +82,34 @@ bool RendererSystem::Init() noexcept
 		}
 	}
 
-	// Get the adapter (video card) description.
 	DXGI_ADAPTER_DESC adapterDesc;
 	result = adapter->GetDesc(&adapterDesc);
 	if (FAILED(result))
 	{		
 		return false;
 	}
-
-	// Store the dedicated video card memory in megabytes.
 	m_videoCardMemory = (int)(adapterDesc.DedicatedVideoMemory / 1024 / 1024);
-
-	// Convert the name of the video card to a character array and store it.
 	unsigned long long stringLength;
 	error = wcstombs_s(&stringLength, m_videoCardDescription, 128, adapterDesc.Description, 128);
 	if (error != 0)
 	{
 		return false;
 	}
-
-	// Release the display mode list.
 	delete[] displayModeList;
 	displayModeList = 0;
-
-	// Release the adapter output.
 	adapterOutput->Release();
 	adapterOutput = 0;
-
-	// Release the adapter.
 	adapter->Release();
 	adapter = 0;
-
-	// Release the factory.
 	factory->Release();
 	factory = 0;
 
-	// Initialize the swap chain description.
 	DXGI_SWAP_CHAIN_DESC swapChainDesc;
 	ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
-
-	// Set to a single back buffer.
 	swapChainDesc.BufferCount = 1;
-
-	// Set the width and height of the back buffer.
 	swapChainDesc.BufferDesc.Width = (unsigned)wndconfig.windowWidth;
 	swapChainDesc.BufferDesc.Height = (unsigned)wndconfig.windowHeight;
-
-	// Set regular 32-bit surface for the back buffer.
 	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-
-	// Set the refresh rate of the back buffer.
 	if (renderconfig.vsync)
 	{
 		swapChainDesc.BufferDesc.RefreshRate.Numerator = numerator;
@@ -150,63 +120,38 @@ bool RendererSystem::Init() noexcept
 		swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
 		swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
 	}
-
-	// Set the usage of the back buffer.
 	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-
-	// Set the handle for the window to render to.
 	swapChainDesc.OutputWindow = windowSystem.GetHWND();
-
-	// Turn multisampling off.
 	swapChainDesc.SampleDesc.Count = 1;
 	swapChainDesc.SampleDesc.Quality = 0;
-
-	// Set to full screen or windowed mode.
 	if (wndconfig.fullscreen)
-	{
 		swapChainDesc.Windowed = false;
-	}
 	else
-	{
 		swapChainDesc.Windowed = true;
-	}
-
-	// Set the scan line ordering and scaling to unspecified.
 	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
 	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
-
-	// Discard the back buffer contents after presenting.
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-
-	// Don't set the advanced flags.
 	swapChainDesc.Flags = 0;
 
-	// Set the feature level to DirectX 11.
 	D3D_FEATURE_LEVEL featureLevel = D3D_FEATURE_LEVEL_11_0;
 
-	// Create the swap chain, Direct3D device, and Direct3D device context.
 	result = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &featureLevel, 1, D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device, NULL, &m_deviceContext);
 	if (FAILED(result))
 	{
 		return false;
 	}
 
-	// Get the pointer to the back buffer.
 	ID3D11Texture2D* backBufferPtr;
 	result = m_swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBufferPtr);
 	if (FAILED(result))
 	{
 		return false;
 	}
-
-	// Create the render target view with the back buffer pointer.
 	result = m_device->CreateRenderTargetView(backBufferPtr, NULL, &m_renderTargetView);
 	if (FAILED(result))
 	{
 		return false;
 	}
-
-	// Release pointer to the back buffer as we no longer need it.
 	backBufferPtr->Release();
 	backBufferPtr = 0;
 
@@ -301,15 +246,11 @@ bool RendererSystem::Init() noexcept
 
 	//std::cout << m_videoCardDescription;
 
-
-
-
 	return true;
 }
 //-----------------------------------------------------------------------------
 void RendererSystem::Close() noexcept
 {
-	// Before shutting down set to windowed mode or when you release the swap chain it will throw an exception.
 	if (m_swapChain)
 	{
 		m_swapChain->SetFullscreenState(false, NULL);
